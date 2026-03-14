761be31a0121934561fbb937802e528a56df50ff
sbolivar <bolivarss1212@gmail.com>
2026-03-14 21:09:13 +0100

    primer ejercicio del 05 hecho

---
+++ cpp05/ex00/Bureaucraft.cpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucraft.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbolivar <sbolivar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/14 19:19:38 by sbolivar          #+#    #+#             */
+/*   Updated: 2026/03/14 21:01:33 by sbolivar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucraft.hpp"
+
+Bureaucrat::Bureaucrat() : name("Ricardo"), grade(1)
+{
+    std::cout << name << " whith the grade " << grade << " was created" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string _name, int _grade) : name(_name), grade(_grade)
+{
+    std::cout << name << " whith the grade " << grade << " was created" << std::endl;
+    if (grade > 150)
+        throw Bureaucrat::GradeTooHighException();
+    if (grade < 1)
+        throw Bureaucrat::GradeTooLowException();
+}
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &other)
+{
+    if (this != &other)
+        grade = other.grade;
+    return (*this);
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &other)
+{
+    *this = other;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << name << " whith the grade " << grade << " was destroyed" << std::endl;
+}
+
+std::string Bureaucrat::getName() const
+{
+    return (name);
+}
+
+int Bureaucrat::getGrade() const
+{
+    return (grade);
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return ("Grade too high");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return ("Grade too low");
+}
+
+void    Bureaucrat::IncrementGrade()
+{
+    grade++;
+}
+void    Bureaucrat::DecrementGrade()
+{
+    grade--;
+}
+
+std::ostream&	operator<<(std::ostream &o, Bureaucrat const &bureaucraft)
+{
+    o << "Bureaucraft: " << bureaucraft.getName() << std::endl;
+    o << "Grade: " << bureaucraft.getGrade() << std::endl;
+	return o;
+}
